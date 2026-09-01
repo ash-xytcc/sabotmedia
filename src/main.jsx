6cb900dfae7794998b93c3c702c81f254dff3072
ash.budding <bjgarr@gmail.com>
2026-09-01 00:48:32 -0700

    Mount AudioLab quick mouse tools

--- src/main.jsx
+++ src/main.jsx
@@ -77,7 +77,9 @@ import './audioLabRobotVoice.js'
 import './audioLabWorkspaceV2.js'
 /* Final AudioLab layer: menus, visible tracks, standard Audacity spatial hierarchy. */
 import './audio-lab-audacity-v5.css'
+import './audio-lab-audacity-tools.css'
 import './audioLabAudacityMenu.js'
+import './audioLabAudacityQuickTools.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
