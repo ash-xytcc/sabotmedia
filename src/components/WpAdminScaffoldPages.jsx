af9827d4dd3291dc9e5ae12a052be09825988c78
ash.budding <bjgarr@gmail.com>
2026-08-31 23:26:54 -0700

    Expose site read aloud voice settings

--- src/components/WpAdminScaffoldPages.jsx
+++ src/components/WpAdminScaffoldPages.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import { AdminFrame } from './AdminRail'
 import { AdminPublicConfigCard } from './AdminPublicConfigCard'
 import { LegacyInfoPageRecovery } from './LegacyInfoPageRecovery'
+import { RobotVoiceSettingsCard } from './RobotVoiceSettingsCard'
 import { getPieces } from '../lib/pieces'
 import { adminRoutes } from '../routing/routes'
 import { publicPageRegistry, withSiteEdit } from '../lib/publicPageRegistry'
@@ -52,6 +53,7 @@ export function SettingsAdminPage() {
           <Link className="button button--primary" to={withSiteEdit('/')}>Edit Live</Link>
         </div>
         <AdminPublicConfigCard />
+        <RobotVoiceSettingsCard />
         <LegacyInfoPageRecovery />
         <section className="wp-meta-box">
           <h2>Operational settings</h2>
