967b78b5f360196b1f639acafb64d875efad346e
ash.budding <bjgarr@gmail.com>
2026-09-03 21:38:37 -0700

    define authoritative Sabot podcast sources

---
+++ functions/api/_lib/sabotPodcastSources.js
@@ -0,0 +1,12 @@
+export const SABOT_PODCAST_SOURCES = Object.freeze([
+  Object.freeze({
+    id: 'molotov-now',
+    title: 'Molotov Now!',
+    feedUrl: 'https://feeds.acast.com/public/shows/sabot-media-molotov',
+  }),
+  Object.freeze({
+    id: 'the-child-and-its-enemies',
+    title: 'The Child and Its Enemies',
+    feedUrl: 'https://feeds.acast.com/public/shows/the-child-and-its-enemies',
+  }),
+])
