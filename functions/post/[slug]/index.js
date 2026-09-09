f84e97940687cae2691f15c70b7d48497d5806e8
ash.budding <bjgarr@gmail.com>
2026-09-09 01:01:18 -0700

    Remove broken trailing-slash article function

--- functions/post/[slug]/index.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url)
-  if (url.pathname.length > 1 && url.pathname.endsWith('/')) {
-    url.pathname = url.pathname.replace(/\/+$/, '')
-    return Response.redirect(url.toString(), 301)
-  }
-  return context.next()
-}
