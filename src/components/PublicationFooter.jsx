d224baad929f928d09bed5103b719895bec082b4
Sabot Media <info@sabot.media>
2026-09-10 23:24:09 -0700

    Update Sabot footer copy

--- src/components/PublicationFooter.jsx
+++ src/components/PublicationFooter.jsx
@@ -63,10 +63,7 @@ export function PublicationFooter() {
       </div>
 
       <div className="publication-footer__bottom">
-        <div>
-          Ⓐ Intellectual property is bullshit - steal this website. Site runs on{' '}
-          <a href="https://github.com/colophon-hub/colophon" target="_blank" rel="noreferrer">Colophon</a>. Build your own.
-        </div>
+        <div>Ⓐ Intellectual property is bullshit - steal this website. Build your own publication.</div>
         {!isChecking && !isAuthenticated ? (
           <Link className="publication-footer__login-link" to="/login">Editor login</Link>
         ) : null}
